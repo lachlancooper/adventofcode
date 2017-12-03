0bae1d6df98657d1cd2c8750cbed05e23c7f1997
Lachlan Cooper <lachlan.cooper@anchor.net.au>
2017-12-04 02:26:15 +1100

    Fix bugs and simplify refactored part 1

--- day3/part1.go
+++ day3/part1.go
@@ -18,52 +18,46 @@ func abs(i int) int {
 	return i
 }
 
-// sqr returns i raised to the power of 2
-func sqr(i int) int {
+// square returns i raised to the power of 2
+func square(i int) int {
 	return i * i
 }
 
-// manhattan returns the distance from the given square
-// to the centre square of a spiral pattern:
-//
-// 37  36  35  34  33  32  31
-// 38  17  16  15  14  13  30
-// 39  18   5   4   3  12  29
-// 40  19   6   1   2  11  28
-// 41  20   7   8   9  10  27
-// 42  21  22  23  24  25  26
-// 43  44  45  46  47  48  49
+// manhattan returns the distance from s
+// to square 1 of a spiral grid:
+// 17  16  15  14  13
+// 18   5   4   3  12
+// 19   6   1   2  11
+// 20   7   8   9  10
+// 21  22  23---> ...
 func manhattan(s int) int {
 	if s == 1 {
 		return 0
 	}
 
-	// layer is absolute distance from grid centre
-	//  2- 9: 1
-	// 10-25: 2
-	// 26-49: 3
-	// 50-81: 4
+	// layer is the chessboard distance of s from square 1
+	// 2   2   2   2   2
+	// 2   1   1   1   2
+	// 2   1   0   1   2
+	// 2   1   1   1   2
+	// 2   2   2   2   2
 	layer := int(math.Ceil(math.Sqrt(float64(s)))) / 2
 
-	// layerstart is number of first square of current layer
-	//  2- 9: 2
-	// 10-25:10
-	// 26-49:26
-	// 50-81:50
-	layerstart := sqr(layer*2-1+layer%2) + 1
+	// prevlayerend is the last square of the previous layer
+	prevlayerend := square(layer*2 - 1)
 
 	// edgelen is number of squares on each edge of current layer
 	edgelen := layer * 2
 
-	edgecentre := (layerstart - 1) + (s-layerstart)/edgelen*edgelen + edgelen/2
+	// nearestaxis is the closest square in the current layer
+	// collinear with square 1, according to the cardinal axes
+	nearestaxis := prevlayerend + (s-prevlayerend)/edgelen*edgelen + edgelen/2
 
-	// first, move along edge to centre square
-	//dist := abs((s-layerstart)%(edgelen) + 1 - layer)
-	dist := abs(s - edgecentre)
-	fmt.Println("Square", s, "is in layer", layer, "which started at", layerstart, ". It takes", dist, "steps to centre of edge (", (s-layerstart)/edgelen, edgecentre, "), then", layer, "steps to grid centre.")
-
-	// then move inward to grid centre, 1 step per layer
+	// first move to nearest axis
+	dist := abs(s - nearestaxis)
+	// then move inward to square 1
 	dist += layer
+
 	return dist
 }
 

9aa3233f4a5b81ebf1743f24133b4aadd9ce7c2e
Lachlan Cooper <lachlancooper@gmail.com>
2017-12-09 00:30:30 +1100

    Solve day 8 part 2

---
+++ day8/part2.go
@@ -0,0 +1,86 @@
+// http://adventofcode.com/2017/day/8
+// part 2
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// memory contains any number of named registers and their values
+var memory = make(map[string]int)
+
+// process instruction i
+func processInstruction(i []string) {
+	// decode instruction
+	register := i[0]
+	operation := i[1]
+	value, _ := strconv.Atoi(i[2])
+	operand1 := memory[i[4]]
+	condition := i[5]
+	operand2, _ := strconv.Atoi(i[6])
+
+	// test condition
+	switch condition {
+	case ">":
+		if !(operand1 > operand2) {
+			return
+		}
+	case ">=":
+		if !(operand1 >= operand2) {
+			return
+		}
+	case "<":
+		if !(operand1 < operand2) {
+			return
+		}
+	case "<=":
+		if !(operand1 <= operand2) {
+			return
+		}
+	case "==":
+		if !(operand1 == operand2) {
+			return
+		}
+	case "!=":
+		if !(operand1 != operand2) {
+			return
+		}
+	}
+
+	// condition passed, perform operation
+	switch operation {
+	case "inc":
+		memory[register] += value
+	case "dec":
+		memory[register] -= value
+	}
+}
+
+// return the largest value in any register
+func largestValue() (max int) {
+	for _, val := range memory {
+		if val > max {
+			max = val
+		}
+	}
+	return
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var largestEver int
+	for scanner.Scan() {
+		processInstruction(strings.Fields(scanner.Text()))
+		largest := largestValue()
+		if largest > largestEver {
+			largestEver = largest
+		}
+	}
+
+	fmt.Println(largestEver)
+}

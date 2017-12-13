7db68b90520c71b6f6665390709e18997f7b8bbe
Lachlan Cooper <lachlancooper@gmail.com>
2017-12-13 19:22:17 +1100

    Solve day 13 part 2
    This is my slowest solution this year by far (>2s execution).
    
    I would be interested in figuring out a faster method than
    bruteforce.

---
+++ day13/part2.go
@@ -0,0 +1,41 @@
+// http://adventofcode.com/2017/day/13
+// part 2
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var firewall = make(map[int]int)
+
+// caught calculates whether or not a packet traversing
+// firewall starting at picosecond s is caught
+func caught(s int) bool {
+	for layer, depth := range firewall {
+		if (layer+s)%(depth*2-2) == 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), ": ")
+		layer, _ := strconv.Atoi(line[0])
+		depth, _ := strconv.Atoi(line[1])
+		firewall[layer] = depth
+	}
+	for i := 0; ; i++ {
+		if !caught(i) {
+			fmt.Println(i)
+			break
+		}
+	}
+}

3fd30b61d28f70fb99eec950bf484cc4ec2e3564
Lachlan Cooper <lachlancooper@gmail.com>
2023-12-15 10:53:21 +1100

    Solve 2023 day12 part2 (incomplete)

---
+++ 2023/day12/part2/part2.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println(solve(scanner))
+}
+
+func solve(scanner *bufio.Scanner) int {
+	total := 0
+
+	// scan each line separately
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+
+		// find the spring map, repeated five times
+		springs := strings.Join([]string{fields[0], fields[0], fields[0], fields[0], fields[0]}, "?")
+
+		// find the expectedVals values, repeated five times
+		expectedVals := []int{}
+		vals := strings.Split(fields[1], ",")
+		for _, v := range vals {
+			count, _ := strconv.Atoi(v)
+			expectedVals = append(expectedVals, count)
+		}
+
+		expected := append(expectedVals, expectedVals...)
+		expected = append(expected, expectedVals...)
+		expected = append(expected, expectedVals...)
+		expected = append(expected, expectedVals...)
+
+		total += arrangements(springs, expected)
+	}
+
+	return total
+}
+
+// arrangements counts the number of valid arrangements of springs in the given input.
+func arrangements(input string, expected []int) int {
+	total := 0
+
+	// fmt.Printf("Testing input %s, with expected vals %v\n", input, expected)
+
+	for i := 0; i < pow(2, strings.Count(input, "?")); i++ {
+		testInput := replace(input, i)
+		if valid(testInput, expected) {
+			total++
+		}
+	}
+
+	return total
+}
+
+// replace replaces ? with # in a pattern matching the variant.
+// This helps to generate every possible combination of springs, e.g.
+//
+//	"???.###"" has 2 ^ 3 = 8 combos
+//	replace("???.###"", 0) == "###.###"
+//	replace("???.###"", 1) == "##..###"
+//	replace("???.###"", 2) == "#.#.###"
+//	replace("???.###"", 3) == "#...###"
+//	replace("???.###"", 4) == ".##.###"
+//	replace("???.###"", 5) == ".#..###"
+//	replace("???.###"", 6) == "..#.###"
+//	replace("???.###"", 7) == "....###"
+func replace(input string, variant int) string {
+	result := input
+
+	// iterate over all ? in input
+	// find next ? in result
+	// if corresponding binary digit of variant is 0, set it to #
+	// otherwise, set it to ?
+	for i := 0; i < strings.Count(input, "?"); i++ {
+		nextOffset := strings.Index(result, "?")
+		var replaceChar string
+		if variant&pow(2, i) == 0 {
+			replaceChar = "#"
+		} else {
+			replaceChar = "."
+		}
+		result = result[:nextOffset] + replaceChar + result[nextOffset+1:]
+	}
+
+	return result
+}
+
+// valid validates whether a given input satisfies the expected group count.
+func valid(input string, expected []int) bool {
+	groups := []int{}
+	for _, v := range strings.Split(input, ".") {
+		if v == "" {
+			continue
+		}
+		groups = append(groups, len(v))
+	}
+
+	result := slices.Equal(groups, expected)
+
+	// fmt.Printf("Checked %s: %v\n", input, result)
+	return result
+}
+
+func pow(x, y int) int {
+	if y == 0 {
+		return 1
+	}
+
+	result := x
+	for ; y > 1; y-- {
+		result *= x
+	}
+
+	return result
+}
---
+++ 2023/day12/part2/part2_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+func Test_solve(t *testing.T) {
+	tests := map[string]struct {
+		scanner *bufio.Scanner
+		want    int
+	}{
+		"example1": {want: 1},
+		// "example": {want: 525152},
+		// "input":   {want: 6935},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			file, err := os.Open("testdata/" + name)
+			if err != nil {
+				t.Errorf("unable to open file %s", name)
+			}
+			scanner := bufio.NewScanner(file)
+			if got := solve(scanner); got != tt.want {
+				t.Errorf("solve() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
